e2053f6d24be5f2103402544429bbff3d418d84f
Al Newkirk <awncorp@cpan.org>
2010-10-15 19:11:16 +0000

    Import of AWNCORP/Dancer-Plugin-DataFu-0.0100 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-DataFu
    gitpan-cpan-version:      0.0100
    gitpan-cpan-path:         AWNCORP/Dancer-Plugin-DataFu-0.0100.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

---
+++ lib/Dancer/Plugin/DataFu.pm
@@ -0,0 +1,178 @@
+# ABSTRACT: Dancer HTML Form and Grid/Table engine with Input Validation
+package Dancer::Plugin::DataFu;
+BEGIN {
+  $Dancer::Plugin::DataFu::VERSION = '0.0100';
+}
+
+use strict;
+use warnings;
+use Dancer qw/:syntax/;
+use Dancer::Plugin;
+use Dancer::Plugin::DataFu::Form;
+use Dancer::Plugin::DataFu::Grid;
+
+my  $settings = plugin_setting;
+
+register 'form' => sub {
+    return Dancer::Plugin::DataFu::Form->new($settings);
+};
+
+register 'grid' => sub {
+    return Dancer::Plugin::DataFu::Grid->new($settings);
+};
+
+
+register_plugin;
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dancer::Plugin::DataFu - Dancer HTML Form and Grid/Table engine with Input Validation
+
+=head1 VERSION
+
+version 0.0100
+
+=head1 SYNOPSIS
+
+    # form rendering and validation
+
+    get 'login' => sub {
+        return form->render('user.login', 'user.password');
+    };
+    
+    post 'login' => sub {
+        my $input = form;
+        redirect '/dashboard' if $input->validate('user.login', 'user.password');
+        return $input->render('user.login', 'user.password');
+    };
+
+=head1 DESCRIPTION
+
+Dancer::Plugin::DataFu is an HTML form and table rendering engine with data
+validation support. All HTML elements are rendered using TT (Template-Toolkit) 
+templates, which can be copied to your application folder and altered as you see fit 
+providing the ultimate in flexibility when rendering HTML and Validating User Input.
+Form rending and input validation is done almost indentically to that of CPAN module
+Oogly::Aagly, please examine that POD (documentation) for a more indepth look at
+configuration.
+
+=head1 CONFIGURATION
+
+    plugins:
+      DataFu:
+        form:
+          templates: views/datafu
+          profiles: data/profiles
+        grid:
+          templates: views/datafu
+          profiles: data/profiles
+
+=head1 FORM PROFILES
+
+    ... myapp/profiles/user.pl
+    
+    # user validation profile
+
+    our $profile = {
+        
+        'login' => {
+            label    => 'user login',
+            required => 1,
+            element  => {
+                type => 'input_text'
+            }
+        },
+        
+        'password' => {
+            label    => 'user password',
+            required => 1,
+            element  => {
+                type => 'input_text'
+            }
+        }
+        
+    };
+
+Profiles are where you would put all of your data input constraints and
+validation rules. Form rending and input validation is done almost indentically
+to that of CPAN module Oogly::Aagly, please examine that POD (documentation) for
+a more indepth look at configuration. All profiles in the specified 'profiles'
+directory are loaded automatically for DRY (don't repeat yourself) purposes,
+allowing you to mix fields from different profiles. The field uses in rendering
+and validation is stored and referenced using the profile name and field name
+together seperated with a single period. i.e. The login field example above would
+be referenced as ...
+
+    form->validate('user.login');
+    form->render('form', '/action', 'user.login');
+
+=head1 GRID PROFILES
+
+    ... myapp/profiles/table/user.pl
+
+    our $profile = {
+        
+        'header'  => 'example table header',
+        'columns' => [
+            {
+                header  => 'ID',
+                bindto  => 'id',
+                element => {
+                    type => 'input_checkbox'
+                }
+            },
+            {
+                header => 'Column One',
+                bindto => 'col1',
+            },
+            {
+                header => 'Column Two',
+                bindto => 'col2',
+            },
+            {
+                header => 'Action',
+                data   => sub {
+                    my ($self, $row) = @_;
+                    return $row->{col1};
+                },
+            }
+        ],
+        'navigation' => sub {
+            my ($self, $dataset) = @_;
+            return 'Found ' . @{$dataset} . ' records';
+        }
+        
+    };
+    
+    my $dataset = [
+        { id => 100, col1 => 'column1a', col2 => 'column2a' },
+        { id => 101, col1 => 'column1b', col2 => 'column2b' },
+        { id => 102, col1 => 'column1c', col2 => 'column2c' },
+        { id => 103, col1 => 'column1d', col2 => 'column2d' },
+        { id => 104, col1 => 'column1e', col2 => 'column2e' },
+        { id => 105, col1 => 'column1f', col2 => 'column2f' },
+        { id => 106, col1 => 'column1g', col2 => 'column2g' },
+        { id => 107, col1 => 'column1h', col2 => 'column2h' },
+        { id => 108, col1 => 'column1i', col2 => 'column2i' },
+        { id => 109, col1 => 'column1j', col2 => 'column2j' },
+    ];
+    grid->render('name', $dataset, 'user');
+
+=head1 AUTHOR
+
+Al Newkirk <awncorp@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by awncorp.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Dancer/Plugin/DataFu/Grid.pm
@@ -0,0 +1,223 @@
+# ABSTRACT: Dancer HTML Grid/Table renderer
+package Dancer::Plugin::DataFu::Grid;
+BEGIN {
+  $Dancer::Plugin::DataFu::Grid::VERSION = '0.0100';
+}
+
+use warnings;
+use 5.008001;
+use Template;
+use Template::Stash;
+use Array::Unique;
+use Dancer::FileUtils;
+use Hash::Merge qw/merge/;
+use Oogly qw/:all !error/;
+use Dancer qw/:syntax !error/;
+use File::ShareDir qw/:ALL/;
+
+{
+    no warnings 'redefine';
+    sub new {
+        my $class    = shift;
+        my $settings = shift;
+        my $profiles = $settings->{grid}->{profiles}
+          || die 'No grid profiles are configured in the config file';
+        my @profiles =
+          glob path( dirname($0), ( split /[\\\/]/, $profiles ), '*.pl' );
+        my $self   = {};
+        my $fields = {};
+    
+        foreach my $profile (@profiles) {
+            next unless $profile;
+
+            die "No such profile: $profile\n"   unless -f $profile;
+            die "Can't read profile $profile\n" unless -r _;
+
+            my ($profile_name) = $profile =~ /[\\\/]([\w\.]+)\.pl/;
+            die "Could not generate a profile name for profile $profile"
+              unless $profile_name;
+
+            $fields->{$profile_name} = do $profile;
+            die "Input profiles didn't return a hash ref: $@\n"
+              unless ref $fields->{$profile_name} eq "HASH";
+        }
+    
+        #my $globule = {};
+        #my $params  = params;
+        #foreach my $key ( keys %{$fields} ) {
+        #    foreach my $field ( keys %{ $fields->{$key} } ) {
+        #        $globule->{"$key.$field"} = $fields->{$key}->{$field};
+        #    }
+        #}
+        
+        $self->{profile} = $fields;
+    
+        my $template_directory =
+          $settings->{grid}->{templates}
+          ? path( dirname($0), $settings->{grid}->{templates} )
+          : module_dir('Dancer::Plugin::DataFu') . "/elements/";
+    
+        $self->{profiles}  = \@profiles;
+        $self->{templates} = { directory => $template_directory };
+    
+        foreach my $tmpl ( glob path $template_directory, '*.tt' ) {
+            my ( $file, $name ) = $tmpl =~ /.*[\\\/]((\w+)\.tt)/;
+            $self->{templates}->{$name} = $file;
+        }
+    
+        die "No TT HTML tempxlates where found under $template_directory"
+          if keys %{ $self->{templates} } <= 1;
+    
+        $self->{settings} = $settings;
+    
+        bless $self, $class;
+        return $self;
+    }
+    
+    sub template {
+        my ( $self, $element, $path ) = @_;
+        return $self->{data}->{templates}->{$element} = $path;
+    }
+}
+
+sub render {
+    my ( $self, $name, $dataset, $profile, @options ) = @_;
+    my $configuration = {};
+       $profile = $self->{profile}->{$profile} if $profile;;
+    
+    # check for grid template vars
+    if ( ref( $options[@options] ) eq "HASH" ) {
+        $configuration = pop @options;
+    }
+
+    my $tempro = sub {
+        my ($file, $args) = @_;
+        my $template    = Template->new(
+            INTERPOLATE => 1,
+            EVAL_PERL   => 1,
+            ABSOLUTE    => 1,
+            ANYCASE     => 1
+        );
+        
+        my $data = undef;
+        
+        $template->process( $file, $args, \$data ) || die $template->error;
+        
+        return $data;
+    };
+
+    my $counter    = 0;
+    my @grid_table = ();
+    my @grid_parts = ();
+    my $tvars      = {
+        col     => undef,
+        row     => undef,
+        grid    => $self,
+        name    => $name,
+        prof    => $profile,
+        vars    => $configuration,
+        data    => $dataset,
+        content => undef
+    };
+    
+    # column headers
+    for (my $i = 0; $i < @{$profile->{columns}}; $i++){
+        $tvars->{col} = $profile->{columns}->[$i];
+        $grid_parts[$counter++] = $tempro->($self->temppath('thead.tt'), $tvars);
+    }
+    
+    $tvars->{content} = join( "\n", @grid_parts );
+    push @grid_table, $tempro->($self->temppath('trow.tt'), $tvars);
+    
+    @grid_parts = ();
+    $counter = 0;
+    
+    # data rows
+    foreach my $row (@{$dataset}) {
+        
+        for (my $i = 0; $i < @{$profile->{columns}}; $i++) {
+            
+            # use dataset value if bindto exists
+            $profile->{columns}->[$i]->{data}
+                =  $row->{$profile->{columns}->[$i]->{bindto}}
+                if defined $profile->{columns}->[$i]->{bindto};
+                
+            $tvars->{row} = $row;
+            $tvars->{col} = $profile->{columns}->[$i];
+            $grid_parts[$counter++] = $tempro->($self->temppath('tdata.tt'), $tvars);
+            
+        }
+        
+        $tvars->{content} = join( "\n", @grid_parts );
+        push @grid_table, $tempro->($self->temppath('trow.tt'), $tvars);
+        
+        @grid_parts = ();
+        $counter = 0;
+        
+    }
+    
+    # table header
+    if (defined $profile->{header}) {
+        $tvars->{col} = $profile->{columns};
+        $tvars->{header} = $tempro->($self->temppath('trow.tt'),
+            { content => $tempro->($self->temppath('theader.tt'), $tvars) }
+        );
+    }
+    
+    # footer
+    if (defined $profile->{navigation}) {
+        $tvars->{col} = $profile->{columns};
+        $tvars->{navigation} = $tempro->($self->temppath('trow.tt'),
+            { content => $tempro->($self->temppath('tnavigation.tt'), $tvars) }
+        );
+    }
+    
+    # output table
+    $tvars->{rows} = join( "\n", @grid_table );
+    return $tempro->($self->temppath('table.tt'), $tvars);
+}
+
+sub templates {
+    my ( $self, $path ) = @_;
+    return $self->{data}->{templates}->{directory} = $path;
+}
+
+# The temppath method concatenates a file with the template directory and
+# returns an absolute path
+sub temppath {
+    my ( $self, $file ) = @_;
+    return path( $self->{templates}->{directory}, $file );
+}
+
+# The dynamic Template::Stash::LIST_OPS has method adds a 'find-in-array'
+# virtual list method for Template-Toolkit
+$Template::Stash::LIST_OPS->{has} = sub {
+    my ( $list, $value ) = @_;
+    return ( grep /$value/, @$list ) ? 1 : 0;
+};
+
+1;
+__END__
+=pod
+
+=head1 NAME
+
+Dancer::Plugin::DataFu::Grid - Dancer HTML Grid/Table renderer
+
+=head1 VERSION
+
+version 0.0100
+
+=head1 AUTHOR
+
+Al Newkirk <awncorp@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by awncorp.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
